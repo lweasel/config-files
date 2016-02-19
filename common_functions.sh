d1e944b3d805f6248bb9a9227f18ef8dc772a3e9
Owen Dando <owen.dando@ed.ac.uk>
2016-02-19 10:14:52 +0000

    Add zhead - head which deals with gzipped files.

--- common_functions.sh
+++ common_functions.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+#
+# A 'less' that can deal with BAM files.
 # From http://martinghunt.github.io/2016/01/25/less-foo.bam.html
+#
+
 original_less=$(which less)
 
-less() {
+function less() {
     regex="[[:graph:]]+\.bam($|[[:space:]])"
 
     if [[ $@ =~ ${regex} ]]; then
@@ -13,3 +17,16 @@ less() {
     fi
 }
 
+#
+# A 'head' which can deal with gzipped files.
+# Adapted from https://gist.github.com/brwnj/5536490
+#
+
+function zhead () {
+    num_args=$#
+    arg_array=("${@}")
+    head_args=("${arg_array[@]:0:${num_args}-1}")
+    file=${arg_array[${num_args}]}
+
+    zcat ${file} | head ${head_args[*]}
+}
